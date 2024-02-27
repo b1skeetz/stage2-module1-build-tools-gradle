2c96db1055df9f7b3bb1d69673fa61ece6a625a6
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-27 22:31:18 +0600

    Task with utils completed

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        int num = Integer.parseInt(str);
+        if(num > 0){
+            return true;
+        }
+        return false;
     }
 }
