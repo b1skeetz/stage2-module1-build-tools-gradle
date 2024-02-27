6900986bfc9a4d0e1529b48a6aafb0eb78b688f5
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-27 23:03:55 +0600

    null and useless 0 cases obtained

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,12 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try{
+            if(str == null){
+                return false;
+            }
+            if(str.startsWith("0")){
+                return false;
+            }
             double num = Double.parseDouble(str);
             return num > 0;
         } catch (NumberFormatException e) {
